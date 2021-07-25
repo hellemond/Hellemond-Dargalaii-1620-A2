819264a0693fcb5ccb0dc9ae283babf0ae00add7
Hellemond Dargalaii <hdargalaii@gmail.com>
2021-07-25 12:27:43 -0700

    change button name in index.html and styles

--- app/app.js
+++ app/app.js
@@ -1,1 +1,10 @@
-const budgetCards = {}
+const budgetCards = {title : '', item1 : '', item2 : '', total : ''}
+
+
+
+const switchPage = document.querySelector('.btn')
+switchPage.addEventListener('click', (ev)=>{
+    console.log('hello')
+})
+
+
