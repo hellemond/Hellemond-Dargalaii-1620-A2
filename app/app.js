ccef73dc75c818ae276aa5e46662ff9fed81cef9
Hellemond Dargalaii <hdargalaii@gmail.com>
2021-07-25 22:42:01 -0700

    dumpster fire

--- app/app.js
+++ app/app.js
@@ -1,41 +1,14 @@
-var budgetArray = [];
-
-createBudgetButton = document.querySelector('.btn1')
-
-viewAllBudgetsButton = document.querySelector('.btn2')
-viewAllBudgetsButton.style.display = 'none'
-
-cardInfo = document.querySelector('.get-card-info')
-cardInfo.style.display = 'none'
 
 
 
 
-createBudgetButton.addEventListener('click', ()=>{
-    cards = document.querySelector('.budget-cards').style.display = 'none'
-    createBudgetButton.style.display = 'none'
-    viewAllBudgetsButton.style.removeProperty("display");
-    cardInfo.style.removeProperty("display");
-})
-
-
-
-
-
-function budgetCards(budgetObj){
-    const card =`
-    <div class='budget-card'>
-        <h2>${budgetObj.title}</h2>
-        <p>${budgetObj.description}</p>
-        <p>${budgetObj.budget}</p>
-    </div>
-    `
-    return card
+function appendCard(){
+    var cardTemp = document.querySelector('get-card-info')
 }
 
-function getBudgetData(){
 
-}
+
+
 
 
 
