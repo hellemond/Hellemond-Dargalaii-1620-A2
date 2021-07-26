99e3c7f921ce34de7520875abb86ff952fe02f58
Hellemond Dargalaii <hdargalaii@gmail.com>
2021-07-25 22:51:28 -0700

    create budget button reveals show all budgets button and budget making input area

--- app/app.js
+++ app/app.js
@@ -1,34 +1,38 @@
+var budgetArray = [];
+
+createBudgetButton = document.querySelector('.create-button')
+
+viewAllBudgetsButton = document.querySelector('.return-button')
+viewAllBudgetsButton.style.display = 'none'
+
+cardInfo = document.querySelector('.get-card-info')
+cardInfo.style.display = 'none'
 
 
 
 
-function appendCard(){
-    var cardTemp = document.querySelector('get-card-info')
+createBudgetButton.addEventListener('click', ()=>{
+    cards = document.querySelector('.budget-cards').style.display = 'none'
+    createBudgetButton.style.display = 'none'
+    viewAllBudgetsButton.style.removeProperty("display");
+    cardInfo.style.removeProperty("display");
+})
+
+
+
+
+
+
+
+
+function budgetCards(budgetObj){
+    const card =`
+    <div class='budget-card'>
+        <h2>${budgetObj.title}</h2>
+        <p>${budgetObj.description}</p>
+        <p>${budgetObj.budget}</p>
+    </div>
+    `
+    return card
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
